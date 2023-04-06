4f9cf4ac7a2fe2c21e4ccb2d6c5146d54b977507
Liviu <liviutoma946@gmail.com>
2023-04-06 19:03:47 +0300

    scroll fix attempt v1.0.0

---
+++ index.js
@@ -0,0 +1,39 @@
+const buttons = document.querySelectorAll(".neon-button");
+const darkModeBtn = document.querySelector('#dark-mode');
+const linkedinText = document.getElementById('email').innerText;
+// const scrollToTop 
+
+
+const copyContent = async () => {
+    try {
+        await navigator.clipboard.writeText(linkedinText);
+        console.log('Copied');
+        alert("Email copied to clipboard")
+    } catch (err) {
+        console.log('Failed: ', err);
+    }
+};
+
+// NEON BUTTONS
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.classList.add("active");
+    setTimeout(() => {
+      button.classList.remove("active");
+      
+    }, 500);
+  });
+});
+
+document.getElementById('email-btn').addEventListener('click', function() {
+    copyContent();
+});
+
+const sun = document.querySelector('.fa-sun');
+const moon = document.querySelector('.fa-moon');
+
+darkModeBtn.addEventListener('click', function() {
+    document.body.classList.toggle('light-mode');
+    sun.classList.toggle('sun');
+    moon.classList.toggle('moon');
+})
